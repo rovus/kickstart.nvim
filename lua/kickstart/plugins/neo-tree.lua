ca76611208d62b57bb1a553eb2d3cc965b430d94
Alexei Suvorov <ps.alexei.suvorov@gmail.com>
2024-06-11 20:28:04 +0300

    moved plugins to separate files, init file cleanup

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -8,16 +8,40 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+    {
+      's1n7ax/nvim-window-picker',
+      version = '2.*',
+      config = function()
+        require('window-picker').setup {
+          filter_rules = {
+            include_current_win = false,
+            autoselect_one = true,
+            -- filter using buffer options
+            bo = {
+              -- if the file type is one of following, the window will be ignored
+              filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+              -- if the buffer type is one of following, the window will be ignored
+              buftype = { 'terminal', 'quickfix' },
+            },
+          },
+        }
+      end,
+    },
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+    { '<C-n>', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['<C-n>'] = 'close_window',
+          ['<cr>'] = {
+            'toggle_node',
+            nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+          },
+          ['<space>'] = 'open',
         },
       },
     },
